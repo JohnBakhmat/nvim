7cb7a50be55ba928a7a63e10ab83223da9a05ffe
JohnBakhmat <johnbakhmat@gmail.com>
2023-04-11 16:19:32 +0300

    Add node_modules and target to ignore. Fix lsp errors vieww

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -62,6 +62,17 @@ end)
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true
+	virtual_text = false,
+	signs = true,
+	float = {
+		border = "single",
+		format = function(diagnostic)
+			return string.format(
+				"%s (%s) [%s]",
+				diagnostic.message,
+				diagnostic.source,
+				diagnostic.code or diagnostic.user_data.lsp.code
+			)
+		end,
+	},
 })
-
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,49 +2,55 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files,{})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({search = vim.fn.input("Grep >") });
+    builtin.grep_string({search = vim.fn.input("Grep >") });
 end)
 
 vim.keymap.set('n','<leader>fb',':Telescope file_browser path=%:p:h select_buffer=true<CR>',{})
 
 
 require("telescope").setup {
-  extensions = {
-    file_browser = {
-      theme = "dropdown",
-      -- disables netrw and use telescope-file-browser in its place
-      hijack_netrw = true,
-      mappings = {
- -- takes effect when `strict` is true
-        ["i"] = {
-          -- your custom insert mode mappings
+    defaults = {
+        file_ignore_patterns = { 
+            "node_modules",
+            "target"
         },
-        ["n"] = {
-          -- your custom normal mode mappings
+    },        
+    extensions = {
+        file_browser = {
+            theme = "dropdown",
+            -- disables netrw and use telescope-file-browser in its place
+            hijack_netrw = true,
+            mappings = {
+                -- takes effect when `strict` is true
+                ["i"] = {
+                    -- your custom insert mode mappings
+                },
+                ["n"] = {
+                    -- your custom normal mode mappings
+                },
+            },
         },
-      },
     },
-  },
 }
 
 require("telescope").load_extension "file_browser"
 
 require'nvim-web-devicons'.setup {
-color_icons = true;
-default = true;
-strict = true;
-override_by_filename = {
-  [".gitignore"] = {
-    icon = "",
-    color = "#f1502f",
-    name = "Gitignore"
-  }
- };
-override_by_extension = {
-  ["log"] = {
-    icon = "",
-    color = "#81e043",
-    name = "Log"
-  }
- };
+    color_icons = true;
+    default = true;
+    strict = true;
+    override_by_filename = {
+        [".gitignore"] = {
+            icon = "",
+            color = "#f1502f",
+            name = "Gitignore"
+        }
+    };
+    override_by_extension = {
+        ["log"] = {
+            icon = "",
+            color = "#81e043",
+            name = "Log"
+        }
+    };
 }
