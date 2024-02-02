f5bad767ed7e90807d5ce1bbe5097dc301aef302
John Bakhmat <johnbakhmat@gmail.com>
2024-02-02 12:23:33 +0300

    fugitive and nerdcommenter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,21 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config =  function()
-			require'nvim-treesitter.configs'.setup {
-				ensure_installed = { "vimdoc", "javascript", "typescript", "c", "lua", "rust" },
-				sync_install = false,
-				auto_install = true,
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = false,
-				},
-			}
+    "nvim-treesitter/nvim-treesitter-context",
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require'nvim-treesitter.configs'.setup {
+                ensure_installed = {
+                    "vimdoc", "javascript", "typescript", "c", "lua", "rust"
+                },
+                sync_install = false,
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false
+                }
+            }
 
-		end
-	}
+        end
+    }
 }
--- lua/plugins/zen.lua
+++ lua/plugins/zen.lua
@@ -1,10 +1,1 @@
-return {
-    {
-  "folke/zen-mode.nvim",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  }
-}
-}
+return {{"folke/zen-mode.nvim", opts = {}}}
