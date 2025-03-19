22546d2554ed500826a7badd51867f9c10063148
John Bakhmat <johnbakhmat@gmail.com>
2025-03-19 11:25:20 +0300

    fix: telescope keymaps

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -3,6 +3,6 @@ vim.keymap.set('n', '<space>fb', '<CMD>Oil<CR>', { desc = '[F]ile [B]rowser' })
 
 -- Telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>fs', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'Telescope help tags' })
